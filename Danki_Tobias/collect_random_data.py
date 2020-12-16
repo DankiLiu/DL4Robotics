7c7b979bb69c8ca52268c97f044b7507fb479646
Danki <liudanqing1996@hotmail.com>
2020-12-16 17:49:01 +0100

    random data collection

--- Danki_Tobias/collect_random_data.py
+++ Danki_Tobias/collect_random_data.py
@@ -1,112 +1,82 @@
+import datetime as dt
 import numpy as np
 import pandas as pd
 from Danki_Tobias.mujoco_envs.reach_environment.reach_demo import ReachEnvJointVelCtrl
 
 folder_path = './data/reach_env/'
+dateTimeObj = dt.datetime.now()
+timestamp = str(dateTimeObj.year) + '-' + str(dateTimeObj.month) + '-' + str(dateTimeObj.day) + '_' + str(dateTimeObj.hour) + '-' + str(dateTimeObj.minute)
 
-
+'''
+Generate #steps_per_rollout samples for every rollout.
+'''
 def random_rollout(steps_per_rollout):
-    env = ReachEnvJointVelCtrl(render=True, crippled=np.array([1, 1, 1, 1, 1, 1, 1, 1]), nsubsteps=10)
+    # Init environment
+    env = ReachEnvJointVelCtrl(render=True, nsubsteps=10)
 
-    states = []
+    state_positions = []
+    state_velocities = []
     actions = []
-    state_deltas = []
+    state_delta_positions = []
+    state_delta_velocities = []
+    """
+    Todo: case without velocity
+    """
     for step in range(steps_per_rollout):
-        old_state = env.agent.state[:14]  # 7 joint position and 7 joint velocity
+        old_state_position = env.agent.state[:7]  # 7 joint positions
+        old_state_velocity = env.agent.state[7:14]    # 7 joint velocities
         action = env.action_space.sample()
         obs, reward, done, _ = env.step(action)
-        new_state = env.agent.state[:14]
-        state_delta = new_state - old_state
+        new_state_positions = env.agent.state[:7]  # 7 new joint positions
+        new_state_velocities = env.agent.state[7:14]  # 7 new joint velocities
+        # Calculate delta using new and old states
+        state_delta_position = new_state_positions - old_state_position
+        state_delta_velocity = new_state_velocities - old_state_velocity
 
-        states.append(old_state)
+        state_positions.append(old_state_position)
+        state_velocities.append(old_state_velocity)
         actions.append(action)
-        state_deltas.append(state_delta)
+        state_delta_positions.append(state_delta_position)
+        state_delta_velocities.append(state_delta_velocity)
 
-    states = np.array(states)
-    actions = np.array(actions)
-    state_deltas = np.array(state_deltas)
-
-    df = pd.DataFrame({'state_position_0': states[:, 0],
-                       'state_position_1': states[:, 1],
-                       'state_position_2': states[:, 2],
-                       'state_position_3': states[:, 3],
-                       'state_position_4': states[:, 4],
-                       'state_position_5': states[:, 5],
-                       'state_position_6': states[:, 6],
-                       'state_velocity_0': states[:, 7],
-                       'state_velocity_1': states[:, 8],
-                       'state_velocity_2': states[:, 9],
-                       'state_velocity_3': states[:, 10],
-                       'state_velocity_4': states[:, 11],
-                       'state_velocity_5': states[:, 12],
-                       'state_velocity_6': states[:, 13],
-                       'action_0': actions[:, 0],
-                       'action_1': actions[:, 1],
-                       'action_2': actions[:, 2],
-                       'action_3': actions[:, 3],
-                       'action_4': actions[:, 4],
-                       'action_5': actions[:, 5],
-                       'action_6': actions[:, 6],
-                       'state_delta_position_0': state_deltas[:, 0],
-                       'state_delta_position_1': state_deltas[:, 1],
-                       'state_delta_position_2': state_deltas[:, 2],
-                       'state_delta_position_3': state_deltas[:, 3],
-                       'state_delta_position_4': state_deltas[:, 4],
-                       'state_delta_position_5': state_deltas[:, 5],
-                       'state_delta_position_6': state_deltas[:, 6],
-                       'state_delta_velocity_0': state_deltas[:, 7],
-                       'state_delta_velocity_1': state_deltas[:, 8],
-                       'state_delta_velocity_2': state_deltas[:, 9],
-                       'state_delta_velocity_3': state_deltas[:, 10],
-                       'state_delta_velocity_4': state_deltas[:, 11],
-                       'state_delta_velocity_5': state_deltas[:, 12],
-                       'state_delta_velocity_6': state_deltas[:, 13]})
+    df = samples_array_to_df(state_positions, state_velocities, state_delta_positions, state_delta_velocities, actions)
     return df
 
 
+def samples_array_to_df(state_positions, state_velocities, state_delta_positions, state_delta_velocities, actions):
+    # 7, 7
+    state_positions = np.array(state_positions)
+    state_velocities = np.array(state_velocities)
+    state_delta_positions = np.array(state_delta_positions)
+    state_delta_velocities = np.array(state_delta_velocities)
+    actions = np.array(actions)
+    # All should have same length
+    assert state_positions.shape[0] == state_velocities.shape[0] == \
+           state_delta_positions.shape[0] == state_delta_velocities.shape[0] \
+           == actions.shape[0]
+
+    dict = {}
+    for i in range(7):
+        dict["state_position_"+ str(i)] = state_positions[:, i]
+        dict["state_velocities_" + str(i)] = state_velocities[:, i]
+        dict["actions_" + str(i)] = actions[:, i]
+        dict["state_delta_positions_" + str(i)] = state_delta_positions[:, i]
+        dict["state_delta_velocities_" + str(i)] = state_delta_velocities[:, i]
+
+    df = pd.DataFrame(dict)
+    return df
+
 def collect_random_samples(number_rollouts, steps_per_rollout):
-    all_rollouts = pd.DataFrame({'state_position_0': [],
-                       'state_position_1': [],
-                       'state_position_2': [],
-                       'state_position_3': [],
-                       'state_position_4': [],
-                       'state_position_5': [],
-                       'state_position_6': [],
-                       'state_velocity_0': [],
-                       'state_velocity_1': [],
-                       'state_velocity_2': [],
-                       'state_velocity_3': [],
-                       'state_velocity_4': [],
-                       'state_velocity_5': [],
-                       'state_velocity_6': [],
-                       'action_0': [],
-                       'action_1': [],
-                       'action_2': [],
-                       'action_3': [],
-                       'action_4': [],
-                       'action_5': [],
-                       'action_6': [],
-                       'state_delta_position_0': [],
-                       'state_delta_position_1': [],
-                       'state_delta_position_2': [],
-                       'state_delta_position_3': [],
-                       'state_delta_position_4': [],
-                       'state_delta_position_5': [],
-                       'state_delta_position_6': [],
-                       'state_delta_velocity_0': [],
-                       'state_delta_velocity_1': [],
-                       'state_delta_velocity_2': [],
-                       'state_delta_velocity_3': [],
-                       'state_delta_velocity_4': [],
-                       'state_delta_velocity_5': [],
-                       'state_delta_velocity_6': []})
+    all_rollouts: pd.DataFrame = pd.DataFrame()
     for rollout in range(number_rollouts):
         df = random_rollout(steps_per_rollout)
-        df['rollout'] = [rollout] * steps_per_rollout
+        df["rollout"] = [rollout] * steps_per_rollout
         all_rollouts = all_rollouts.append(df)
 
     all_rollouts = all_rollouts.reset_index(drop=True)
-    all_rollouts.to_csv(folder_path + 'random_samples.csv')
+    print(all_rollouts.head())
+
+    all_rollouts.to_csv(folder_path + 'random_samples_' + str(timestamp) + '.csv')
 
 
-collect_random_samples(10, 50)
+collect_random_samples(2, 50)
