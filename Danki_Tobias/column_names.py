2dd067dbcbc343c1c2f80cfd70ae573d66aa46d1
Tobias <uxecu@student.kit.edu>
2021-03-01 14:36:20 +0100

    Adds new data reader class

--- Danki_Tobias/column_names.py
+++ Danki_Tobias/column_names.py
@@ -1,9 +1,17 @@
+action_columns = ['actions_0', 'actions_1', 'actions_2', 'actions_3', 'actions_4', 'actions_5', 'actions_6']
+
 state_columns = ['state_positions_0', 'state_positions_1', 'state_positions_2', 'state_positions_3',
                  'state_positions_4', 'state_positions_5', 'state_positions_6', 'state_velocities_0',
                  'state_velocities_1', 'state_velocities_2', 'state_velocities_3', 'state_velocities_4',
                  'state_velocities_5', 'state_velocities_6']
 
-action_columns = ['actions_0', 'actions_1', 'actions_2', 'actions_3', 'actions_4', 'actions_5', 'actions_6']
+label_columns = ['label_positions_0', 'label_positions_1', 'label_positions_2', 'label_positions_3',
+                 'label_positions_4', 'label_positions_5', 'label_positions_6', 'label_velocities_0',
+                 'label_velocities_1', 'label_velocities_2', 'label_velocities_3', 'label_velocities_4',
+                 'label_velocities_5', 'label_velocities_6']
+
+state_columns_position_only = ['state_0', 'state_1', 'state_2', 'state_3', 'state_4', 'state_5', 'state_6']
+label_columns_position_only = ['label_0', 'label_1', 'label_2', 'label_3', 'label_4', 'label_5', 'label_6']
 
 delta_columns = ['state_delta_positions_0', 'state_delta_positions_1', 'state_delta_positions_2',
                  'state_delta_positions_3', 'state_delta_positions_4', 'state_delta_positions_5',
