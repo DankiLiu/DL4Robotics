a00e89c8fef67fbf7fa2feaacd7060ca0a43189f
Danki <liudanqing1996@hotmail.com>
2021-03-10 18:51:29 +0100

    Solve comflict

--- Danki_Tobias/data_scripts/collect_experiment_data.py
+++ Danki_Tobias/data_scripts/collect_experiment_data.py
@@ -22,7 +22,7 @@ class AgentJoints():
 
 
 # Test: collect random data with disabled_joints_01
-if __name__ == '__main__':
+if __name__ != '__main__':
     num_rollouts_train, num_rollouts_val, steps_per_rollout_train, steps_per_rollout_val = data_collection_params()
     """
     agents_joints = [AgentJoints(np.array([1, 1, 1, 1, 1, 1, 1, 1]), "non-crippled"),
@@ -31,7 +31,7 @@ if __name__ == '__main__':
                      AgentJoints(np.array([0.5, 1, 1, 1, 0, 0.3, 1, 1]), "056_partial"),
                      AgentJoints(np.array([0.8, 0.9, 0.6, 0.8, 0.5, 1, 0.7, 1]), "123457_partial"),
                      AgentJoints(np.array([0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 1]), "1234567_partial")]
-    """
+    
     agents_joints = [AgentJoints(np.array([1, 1, 1, 1, 1, 1, 1, 1]), "non_crippled_test"),
                      AgentJoints(np.array([1, 1, 1, 0, 1, 1, 1, 1]), "fourth_disabled_test"),
                      AgentJoints(np.array([1, 1, 1, 1, 1, 1, 0, 1]), "seventh_disabled_test"),
@@ -51,7 +51,7 @@ if __name__ == '__main__':
                                            agent,
                                            reach_env_data_path + "/" + agent_joints.name)
         data_collector.perform_data_collection()
-
+    """
 data_path = str(current_path.parent) + f"/data/{data_type}/random_samples"
 
 state_delta = True
