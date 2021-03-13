45362498dcbd81b8c5e8ce39cfbbec1ea132c090
Tobias <uxecu@student.kit.edu>
2021-03-13 11:45:56 +0100

    adds plot comparing Model B

--- Danki_Tobias/evaluation/analyse_rewards.py
+++ Danki_Tobias/evaluation/analyse_rewards.py
@@ -5,9 +5,9 @@ data_type_options = ['position', 'position_deltas', 'position_and_velocity', 'po
 train_on_options = ['non_crippled', 'multiple_envs']
 algorithms = ['normal', 'meta', 'online_adaptation']
 
-data_type = data_type_options[0]
-train_on = train_on_options[0]
-algorithm = algorithms[0]
+data_type = data_type_options[3]
+train_on = train_on_options[1]
+algorithm = algorithms[2]
 
 file_name = f'../data/{data_type}/on_policy/trained_on_{train_on}/{algorithm}/rewards.csv'
 rewards = pd.read_csv(file_name)
